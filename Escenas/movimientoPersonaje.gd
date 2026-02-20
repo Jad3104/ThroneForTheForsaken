ac778c8485f0bcaaa00c89f88066d84f4c232d7f
Wayden03 <104948092+Wayden03@users.noreply.github.com>
2026-02-19 20:26:39 -0600

    Enemigo se mueve y se queda pegado

--- Escenas/movimientoPersonaje.gd
+++ Escenas/movimientoPersonaje.gd
@@ -3,19 +3,17 @@ extends CharacterBody2D
 const JUMP_VELOCITY = -400.0
 @export var maxSpeed=300
 
-func _physics_process(delta: float) -> void:
-	var input_vector=Vector2.ZERO
-	input_vector.x=Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
-	input_vector.y=Input.get_action_strength("ui_down")-Input.get_action_strength("ui_up")
-	input_vector=input_vector.normalized()
-
-	if input_vector !=Vector2.ZERO:
-		velocity=input_vector
-	else:
-		velocity = Vector2.ZERO
-	move_and_collide(velocity*delta*maxSpeed)
+func _ready():
+	add_to_group("player")
 	
+func _physics_process(delta: float) -> void:
+	var input_vector = Vector2.ZERO
+	
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	input_vector = input_vector.normalized()
 
-
+	velocity = input_vector * maxSpeed
 
 	move_and_slide()
